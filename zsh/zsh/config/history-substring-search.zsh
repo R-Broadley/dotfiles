e0d8cefe3fe554b328ef73b19a6d289e6a75e341
Rob Broadley <git@rbroadley.co.uk>
2019-04-19 11:29:31 +0100

    Turned on vi mode for zsh and tmux

--- zsh/zsh/config/history-substring-search.zsh
+++ zsh/zsh/config/history-substring-search.zsh
@@ -1,4 +1,8 @@
 bindkey "$terminfo[kcuu1]" history-substring-search-up
 bindkey "$terminfo[kcud1]" history-substring-search-down
+bindkey -M viins "$terminfo[kcuu1]" history-substring-search-up
+bindkey -M viins "$terminfo[kcud1]" history-substring-search-down
+bindkey -M vicmd "$terminfo[kcuu1]" history-substring-search-up
+bindkey -M vicmd "$terminfo[kcud1]" history-substring-search-down
 
 setopt HIST_FIND_NO_DUPS
