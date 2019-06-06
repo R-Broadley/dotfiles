62ba80c5f471c9b56ea7e3b02fc9547de76a8932
Rob Broadley <git@rbroadley.co.uk>
2019-06-06 13:08:10 +0100

    Updated zsh history search key bindings for wider compatibility

--- zsh/zsh/config/history-substring-search.zsh
+++ zsh/zsh/config/history-substring-search.zsh
@@ -1,9 +1,9 @@
-bindkey "$terminfo[kcuu1]" history-substring-search-up
-bindkey "$terminfo[kcud1]" history-substring-search-down
-bindkey -M viins "$terminfo[kcuu1]" history-substring-search-up
-bindkey -M viins "$terminfo[kcud1]" history-substring-search-down
-bindkey -M vicmd "$terminfo[kcuu1]" history-substring-search-up
-bindkey -M vicmd "$terminfo[kcud1]" history-substring-search-down
+bindkey "^[[A" history-substring-search-up
+bindkey "^[[B" history-substring-search-down
+bindkey -M viins "^[[A" history-substring-search-up
+bindkey -M viins "^[[B" history-substring-search-down
+bindkey -M vicmd "^[[A" history-substring-search-up
+bindkey -M vicmd "^[[B" history-substring-search-down
 
 setopt HIST_FIND_NO_DUPS
 
