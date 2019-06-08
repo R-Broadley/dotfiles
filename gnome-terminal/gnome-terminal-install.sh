a9ec92d746c65baeba2fae75b1dc49ab8cc331fb
Rob Broadley <git@rbroadley.co.uk>
2019-06-08 10:01:15 +0100

    Fixed .profile ignored if .bash_profile exists
    Changed indent in *.sh files to tabs and set editorconfig to
    reflect this. The change allows proper indentation when writing
    multi line strings to file.

--- gnome-terminal/gnome-terminal-install.sh
+++ gnome-terminal/gnome-terminal-install.sh
@@ -5,17 +5,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Get list of current profiles
 # > list profiles (they start with :), replace :/ with ',
-#   replace new lines with comma, remove trailing comma
+#	replace new lines with comma, remove trailing comma
 PROFILE_LIST=$(dconf list /org/gnome/terminal/legacy/profiles:/ | grep "^:" | tr :/ \' | tr "\n" , | sed 's/,*$//g')
 
 # If profile not in profiles list, add it
 ONEDARK_ID="ac7a6fc8-f5df-47f6-84fb-dbb4e30350eb"
 if [[ $PROFILE_LIST != *"$ONEDARK_ID"* ]]; then
-    if [[ $PROFILE_LIST == "" ]]; then
-        PROFILE_LIST="'$ONEDARK_ID'"
-    else
-        PROFILE_LIST="$PROFILE_LIST,'$ONEDARK_ID'"
-    fi
+	if [[ $PROFILE_LIST == "" ]]; then
+		PROFILE_LIST="'$ONEDARK_ID'"
+	else
+		PROFILE_LIST="$PROFILE_LIST,'$ONEDARK_ID'"
+	fi
 fi
 
 # Load template and set new profile list
--- install.sh
+++ install.sh
@@ -3,37 +3,56 @@
 # Get path of repo directory (the one containing this script)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+
 # Update submodules
 ( cd $DIR && git submodule update --init --recursive )
 
+
 # Functions
 ensure_line_in_file () {
-    grep -qF "$1" "$2" || ( echo "$1" >> "$2" && return 10 )
+	grep -qF "$1" "$2" || ( echo "$1" >> "$2" && return 10 )
 }
 
+
 # General settings
-ensure_line_in_file "export VISUAL=nvim" ~/.profile #&& echo "export EDITOR=\$VISUAL" >> ~/.profile
+# Ensure .profile exists
+touch ~/.profile
+# Editor
+ensure_line_in_file "export VISUAL=nvim" ~/.profile
 status=$?
 if [[ $status -eq 10 ]]; then
-    # if added VISUAL=nvim also add EDITOR=VISUAL after
-    echo "export EDITOR=\$VISUAL" >> ~/.profile
+	# if added VISUAL=nvim also add EDITOR=VISUAL after
+	echo "export EDITOR=\$VISUAL" >> ~/.profile
 else
-    # check VISUAL=EDITOR set
-    ensure_line_in_file "export EDITOR=\$VISUAL" ~/.profile
+	# check VISUAL=EDITOR set
+	ensure_line_in_file "export EDITOR=\$VISUAL" ~/.profile
 fi
+# .profile sourced in .bash_profile if it exists
+if [ -f ~/.bash_profile ]; then
+	grep -qF "source ~/.profile" ~/.bash_profile || cat >> ~/.bash_profile <<-"EOF"
+
+		# Get shell independant variables, aliases and functions
+		if [ -f ~/.profile ]; then
+		    source ~/.profile
+		fi
+	EOF
+fi
+
 
 # Set up gnome-terminal (if dconf installed)
 command -v dconf >/dev/null 2>&1
 status=$?
 if [[ $status -eq 0 ]]; then
-    $DIR/gnome-terminal/gnome-terminal-install.sh
+	$DIR/gnome-terminal/gnome-terminal-install.sh
 fi
+
 
 # Set up tmux
 ln -s -f $DIR/tmux/.tmux.conf ~
 ln -s -f $DIR/tmux/.tmux.conf.local ~
 TMUXX='alias tmuxx="tmux new-session -A -s"'
-grep -q "$TMUXX" ~/.bashrc || echo "$TMUXX" >> ~/.bashrc
+ensure_line_in_file "$TMUX" ~/.bashrc
+
 
 # Set up neovim
 mkdir -p ~/.config/nvim
@@ -44,13 +63,16 @@ mkdir -p $DEIN_DIR
 ln -s $DIR/neovim/dein.vim $DEIN_DIR
 echo "To get neovim up and running open it and run :call dein#install()"
 
+
 # Set up zsh
 $DIR/zsh/zsh_install.py
+
 
 # Fonts
 mkdir -p ~/.fonts
 rm -rf ~/.fonts/nerd-fonts || true
 ln -s $DIR/fonts/nerd-fonts ~/.fonts/
+
 
 # Default editorconfig (for any files below users home)
 ln -s -f $DIR/.editorconfig ~
